b3329064e1b825ba3aeb35a7c00cdd4bc5cabd89
JuanYapur <cr.juanpabloyapur@gmail.com>
2024-06-09 23:58:38 -0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
 
+
 function App() {
   
 
   return (
     <>
-      <p>hola mundo</p>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      hola mundos
+    </div>
+  )
+}
+
+export default Navbar
