c873bb081a3937effe7c86501cf21350ed1d022c
JuanYapur <cr.juanpabloyapur@gmail.com>
2024-06-10 00:48:08 -0300

    feat-Navbar-add props

--- src/components/generals/Navbar/Navbar.jsx
+++ src/components/generals/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
+import Link from "../Link/Link";
+
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          Navbar
+          Rick & Morty
         </a>
         <button
           className="navbar-toggler"
@@ -19,52 +21,9 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" aria-disabled="true">
-                Disabled
-              </a>
-            </li>
+            <Link label={"Inicio"}/>
+            <Link label={"Episodios"}/>
+            <Link label={"Personajes"}/>
           </ul>
           <form className="d-flex" role="search">
             <input
@@ -74,7 +33,7 @@ const Navbar = () => {
               aria-label="Search"
             />
             <button className="btn btn-outline-success" type="submit">
-              Search
+              Buscar
             </button>
           </form>
         </div>
