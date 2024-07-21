52ef12ce2173abb0c99aef3d1ff6af64025be9cf
JuanYapur <cr.juanpabloyapur@gmail.com>
2024-07-21 19:38:04 -0300

    feat-characterdetail.jsx-route dom

--- src/components/generals/Card/Cards.jsx
+++ src/components/generals/Card/Cards.jsx
@@ -1,9 +1,12 @@
 import styles from '/src/styles.module.css'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const Cards = ({character:{name,image,species,status}}) => {
+const Cards = ({character:{name,image,species,status,id}}) => {
   const {cardText}=styles
+  const navigate = useNavigate();
   return (
-    <div className="card w-25 h-25 m-2 col-3">
+    <div className="card w-25 h-25 m-2 col-3" onClick={()=>{navigate(`/character/${id}`)}}>
         <img className="card-img-top" src={image} alt="Card image cap"/>
         <div className="card-body">
           <h5 className="card-title">Nombre: {name}</h5>
@@ -17,3 +20,5 @@ const Cards = ({character:{name,image,species,status}}) => {
 }
 
 export default Cards
+
+
--- src/components/generals/CharactersDetails/CharactersDetails.jsx
+++ src/components/generals/CharactersDetails/CharactersDetails.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useState,useEffect } from 'react';
+import axios from 'axios';
 
 const CharactersDetails = () => {
-  return (
-    <div>
-      rey
-    </div>
+  const [character,setCharacter] = useState({})
+
+  const {id}  = useParams();
+  const fechdata = async () => {
+    try {
+      const {data} = await axios(`https://rickandmortyapi.com/api/character/${id}`);
+      setCharacter(data)
+      console.log(data)
+    }
+    catch(error) {console.log('error')}
+  }
+  console.log(character)
+  useEffect(() => {
+    fechdata();
+  }, [id])
+  return (  
+    <div>rey</div>
   )
 }
 
